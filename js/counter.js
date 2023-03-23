a8d4a4b02f67ec55f2dfb021bd2c294faac75c48
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-03-23 20:27:12 +0200

    fix counter issue

--- js/counter.js
+++ js/counter.js
@@ -1,30 +1,35 @@
 class Counter {
-  constructor(valus, time) {
-    this.valus = valus;
+  constructor(values, time) {
+    this.values = values;
     this.time = time;
   }
 
   count() {
-    this.valus.forEach((value) => {
-      let startValu = 0;
+    this.values.forEach((value) => {
+      let startValue = 0;
       let endValue = parseInt(value.getAttribute("data-val"));
       let duration = Math.floor(this.time / endValue);
       let counter = setInterval(function () {
-        startValu++;
-        value.textContent = startValu;
-        if (startValu == endValue) clearInterval(counter);
+        startValue++;
+        value.textContent = startValue;
+        if (startValue == endValue) clearInterval(counter);
       }, duration);
     });
   }
 }
 
+let counter = new Counter(document.querySelectorAll(".counter"), 4000);
 let section = document.querySelector(".works");
 let started = false;
-onscroll = function () {
+
+function play() {
   if (window.scrollY >= section.offsetTop - 500) {
     if (!started) {
-      new Counter(document.querySelectorAll(".counter"), 4000).count();
+      counter.count();
       started = true;
     }
   }
-};
+}
+
+window.addEventListener("scroll", play);
+window.addEventListener("load", play);
